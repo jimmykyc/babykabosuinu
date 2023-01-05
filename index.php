a92d8219271be3128b43fbd7ac3460aa0a9fa243
Jimmy Dev <81708489+jimmykyc@users.noreply.github.com>
2023-01-06 00:57:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en-gb">
+	<title type="text">Kabosu</title>
+	<subtitle type="text">Dedication to KABOSU for all the doge &amp; meme lovers!</subtitle>
+	<link rel="alternate" type="text/html" href="https://welovekabosu.com"/>
+	<id>https://welovekabosu.com/index.php</id>
+	<updated>2023-01-05T08:54:13+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="https://welovekabosu.com/index.php?format=feed&amp;type=atom"/>
+</feed>
---
+++ index1.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Kabosu</title>
+		<description><![CDATA[Dedication to KABOSU for all the doge & meme lovers!]]></description>
+		<link>https://welovekabosu.com/index.php</link>
+		<lastBuildDate>Thu, 05 Jan 2023 08:54:13 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="https://welovekabosu.com/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+	</channel>
+</rss>
